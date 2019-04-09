14eab86c72b439695e6034352018f4374c7210e9
joel.chacon <joel.chacon@cimat.mx>
2019-04-09 12:03:19 -0500

    ..

--- Imagenes_Biomedicas/Materia/Proyecto/min.m
+++ Imagenes_Biomedicas/Materia/Proyecto/min.m
@@ -13,21 +13,23 @@ I=skel;
 
 imPoints = I;
 imPoints(:) = 0;
-for i = 8:components.NumObjects
+for i = 1:components.NumObjects
     tmp = zeros(size(I));
     tmp(components.PixelIdxList{i})=1;
    [r,c] = find(tmp);
-   result = douglas_peucker([transpose(c);transpose(r)], 5);
-%   imPoints(sub2ind(size(I),result(2,:),result(1,:))) = 1;  
-   outImg = ArteryModeling(result, I);
-   imshow(outImg);
-   break;
+   result = [result douglas_peucker([transpose(c);transpose(r)], 5)];
+   %outImg = ArteryModeling(result, I);
+   %imshow(outImg);
+   %break;
 end
 
 %dilatedImage = imdilate(I,strel('disk',15));
 %thinedImage = bwmorph(dilatedImage,'thin',inf);
+%imPoints(sub2ind(size(I),result(2,:),result(1,:))) = 1;
+outImg = ArteryModeling(result, I)
 
-%imshow(thinedImage)
+
+%imshow(imPoints)
 %Vq = interp2(I,5);
 %figure
 %imagesc(Vq);
